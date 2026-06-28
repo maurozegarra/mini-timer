68a0f7feb14052828f14352d94a1e85d063a4b5a
maurozegarra <maurozegarra@users.noreply.github.com>
2026-06-28 01:25:09 -0500

    5th accent color to grey; Now Bar icon to hourglass (larger glyph); 1.0.102

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.minitimer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 101
-        versionName = "1.0.101"
+        versionCode = 102
+        versionName = "1.0.102"
     }
 
     buildTypes {
