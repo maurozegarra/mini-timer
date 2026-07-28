e5d42926864ac8d046892664b6431fdcf6dd4eb1
Master <mauro.zegarra@gmail.com>
2026-07-28 10:41:13 -0500

    chore: subir versión a 1.0.228

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.minitimer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 221
-        versionName = "1.0.221"
+        versionCode = 228
+        versionName = "1.0.228"
     }
 
     buildTypes {
